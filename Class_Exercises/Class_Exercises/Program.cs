a9687653253bce169aec8a852f1ad3791aeb33c4
Regan Barton <9996805@student.toiohomai.ac.nz>
2018-02-27 16:29:11 +1300

    Added Hello World Code

--- Class_Exercises/Class_Exercises/Program.cs
+++ Class_Exercises/Class_Exercises/Program.cs
@@ -10,6 +10,8 @@ namespace Class_Exercises
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
